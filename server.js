43a2f1a6b03d33820dc90d7871d4944106e3aa25
fixcer <toan@fixcer.me>
2020-10-07 12:57:31 +0700

    Hello world

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-akd;
+// import express from 'express';
+var express = require('express');
+
+const app = express();
+
+const host = 'localhost';
+const port = 8000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!!</h1>');
+});
+
+app.listen(port, host, () => {
+  console.log(`Hello Fixcer, i'm running at ${host}:${port}/`);
+});
