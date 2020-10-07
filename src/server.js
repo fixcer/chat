5325271d71829bad429dd66531f5d56ccd54ca2d
fixcer <toan@fixcer.me>
2020-10-07 17:09:19 +0700

    Them cac trang tinh va thiet lap duong dan

--- src/server.js
+++ src/server.js
@@ -1,31 +1,26 @@
 import express from 'express';
 import ConnectDB from './config/connectDB';
-import ContactModel from './models/ContactModel';
+import configViewEngine from './config/viewEngine';
 
+// Initial app
 const app = express();
 
 // Connect to MongoDB
 ConnectDB();
 
-const host = 'localhost';
-const port = 8000;
-
-app.get('/test', async (req, res) => {
-  try {
-    let item = {
-      userId: '191098',
-      contactId: '19101998',
-    };
-
-    let contact = await ContactModel.createNew(item);
-    res.send(contact);
-  } catch (err) {}
-});
+// Config view engine
+configViewEngine(app);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!!</h1>');
+  return res.render('main/master');
 });
 
-app.listen(process.env.PORT, process.env.host, () => {
-  console.log(`Hello Fixcer, i'm running at ${host}:${port}/`);
+app.get('/login-register', (req, res) => {
+  return res.render('auth/loginRegister');
+});
+
+app.listen(process.env.APP_PORT, process.env.APP_HOST, () => {
+  console.log(
+    `Hello Fixcer, i'm running at ${process.env.APP_HOST}:${process.env.APP_PORT}/`
+  );
 });
