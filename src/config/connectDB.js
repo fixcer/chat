7f13969a8f190958ef29c1c56cc776299f86c71f
fixcer <toan@fixcer.me>
2020-10-08 08:32:03 +0700

    Browser-sync

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -2,25 +2,22 @@ import mongoose from 'mongoose';
 import bluebird from 'bluebird';
 
 const connectDB = () => {
-  mongoose.Promise = bluebird;
-
-  const URI = `${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-
-  return mongoose.connect(URI, { useMongoClient: true });
-
-  // const mongoUri =
-  //   'mongodb+srv://admin:password@cluster0.livbm.mongodb.net/chat?retryWrites=true&w=majority';
-  // mongoose.connect(mongoUri, {
-  //   useNewUrlParser: true,
-  //   useCreateIndex: true,
-  //   useUnifiedTopology: true,
-  // });
-  // mongoose.connection.on('connected', () => {
-  //   console.log('Connected to mongo instance');
-  // });
-  // mongoose.connection.on('error', (err) => {
-  //   console.error('Error connecting to mongo', err);
-  // });
+  // mongoose.Promise = bluebird;
+  // const URI = `${process.env.DB_CONNECTION}://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+  // return mongoose.connect(URI, { useMongoClient: true });
+  const mongoUri =
+    'mongodb+srv://admin:admin@cluster0.xwbcv.mongodb.net/chat?retryWrites=true&w=majority';
+  mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+  mongoose.connection.on('connected', () => {
+    console.log('Connected to mongo instance');
+  });
+  mongoose.connection.on('error', (err) => {
+    console.error('Error connecting to mongo', err);
+  });
 };
 
 module.exports = connectDB;
