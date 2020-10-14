4aeddb01aefc5e4988112505b10507d033131548
fixcer <toan@fixcer.me>
2020-10-14 16:54:40 +0700

    Hoan thanh dang ki tai khoan qua email

--- lang/vi.js
+++ lang/vi.js
@@ -5,3 +5,15 @@ export const transValidation = {
     'Mật khẩu phải chứa ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, chữ số và ký tự đặc biệt',
   password_confirmation_incorrect: 'Hai mật khẩu phải trùng khớp',
 };
+
+export const transErrors = {
+  email_in_use: 'Email đã được sử dụng',
+  account_removed: 'Tài khoản đã bị xóa',
+  account_not_active: 'Tài khoản chưa được kích hoạt',
+};
+
+export const transSuccess = {
+  userCreated: (userEmail) => {
+    return `Tài khoản <strong>${userEmail}</strong> đã được tạo. Vui lòng kiểm tra email để kích hoạt tài khoản`;
+  },
+};
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,11 +1,16 @@
 import { validationResult } from 'express-validator/check';
+import { authService } from '../services/index';
 
 const getAuth = (req, res) => {
-  return res.render('auth/index');
+  return res.render('auth/index', {
+    errors: req.flash('errors'),
+    success: req.flash('success'),
+  });
 };
 
-const postRegister = (req, res) => {
+const postRegister = async (req, res) => {
   const errorArray = [];
+  const successArray = [];
   const validationErrors = validationResult(req);
 
   if (!validationErrors.isEmpty()) {
@@ -14,11 +19,24 @@ const postRegister = (req, res) => {
       errorArray.push(error.msg);
     });
 
-    console.log(errorArray);
-    return;
+    req.flash('errors', errorArray);
+    return res.redirect('/auth');
   }
 
-  console.log(req.body);
+  try {
+    const user = await authService.register(
+      req.body.email,
+      req.body.gender,
+      req.body.password
+    );
+    successArray.push(user);
+    req.flash('success', successArray);
+    return res.redirect('/auth');
+  } catch (error) {
+    errorArray.push(error);
+    req.flash('errors', errorArray);
+    return res.redirect('/auth');
+  }
 };
 
 module.exports = { getAuth, postRegister };
