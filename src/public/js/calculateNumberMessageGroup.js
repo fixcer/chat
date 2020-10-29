5e152e840e4b11c955e231e3b81539db37a04c34
fixcer <toan@fixcer.me>
2020-10-29 08:42:20 +0700

    fix so tin nhan trong group

--- src/public/js/calculateNumberMessageGroup.js
+++ src/public/js/calculateNumberMessageGroup.js
@@ -1,11 +1,11 @@
 function increaseNumberMessageGroup(divId) {
-  let currentValue = +$(`right[data-chat = ${divId}]`)
-    .find('span.show-number-members')
+  let currentValue = +$(`.right[data-chat = ${divId}]`)
+    .find('span.show-number-messages')
     .text();
 
   currentValue += 1;
 
-  $(`right[data-chat = ${divId}]`)
-    .find('span.show-number-members')
+  $(`.right[data-chat = ${divId}]`)
+    .find('span.show-number-messages')
     .html(currentValue);
 }
