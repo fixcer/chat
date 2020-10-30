00e51069a2e6b6c2d56a6865354088dd8ab14a6a
fixcer <toan@fixcer.me>
2020-10-30 13:17:12 +0700

    Config DB local

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -1,17 +1,43 @@
+// import mongoose from 'mongoose';
+
+// const connectDB = () => {
+//   const mongoUri =
+//     'mongodb+srv://admin:admin@cluster0.xwbcv.mongodb.net/chat?retryWrites=true&w=majority';
+//   mongoose.connect(mongoUri, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useUnifiedTopology: true,
+//     useFindAndModify: false,
+//   });
+//   mongoose.connection.on('connected', () => {
+//     console.log('Connected to mongo instance');
+//   });
+//   mongoose.connection.on('error', (err) => {
+//     console.error('Error connecting to mongo', err);
+//   });
+// };
+
+// module.exports = connectDB;
+
 import mongoose from 'mongoose';
+import bluebird from 'bluebird';
 
 const connectDB = () => {
-  const mongoUri =
-    'mongodb+srv://admin:admin@cluster0.xwbcv.mongodb.net/chat?retryWrites=true&w=majority';
+  mongoose.Promise = bluebird;
+
+  const mongoUri = 'mongodb://localhost:27017/chat';
+
   mongoose.connect(mongoUri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
   });
+
   mongoose.connection.on('connected', () => {
     console.log('Connected to mongo instance');
   });
+
   mongoose.connection.on('error', (err) => {
     console.error('Error connecting to mongo', err);
   });
